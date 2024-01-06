e6d422ee54b6d5653e7385d9448e863eade18977
jozwiaka <arkadiuszjozwiakdev@gmail.com>
2024-01-06 12:21:21 +0100

    Update

--- src/App-Common/Config.hpp
+++ src/App-Common/Config.hpp
@@ -16,8 +16,10 @@ namespace Config
     namespace Networking
     {
         // If a server is listening on 0.0.0.0 for a certain port, it will accept connections from any external IP address or from the localhost. When a server binds to 127.0.0.1, it will only accept connections that originate from the same machine (the local loopback interface). It's used for communication between processes on the same machine
-        // Server listens to 0.0.0.0:8080
-        // Client connects to 127.0.0.1:8080
+        // [Local/Docker-Server] Server listens on 0.0.0.0:8080
+        // [Local-Client] Client connects to 127.0.0.1:8080
+        // [Docker-Client] Client connects to 172.20.0.1:8080 (or directly to 172.20.0.3:8080)
+        // 172.20.0.1 - Address from a Docker bridge network. Used as the gateway for containers within that Docker bridge network
         const std::string_view SERVER_IP_ADDRESS = (std::getenv("DOCKER_CONTAINER") != nullptr) ? "172.20.0.1" : "127.0.0.1";
         constexpr int PORT = 8080;
         constexpr int BUFFER_SIZE = 1024;
